bcf3b9957bf76a5d745a3ce10ec6e9acfa2386f1
Rishabh Bector <bector2013@gmail.com>
2018-08-26 12:38:41 -0500

    collisions and gravity

--- main.go
+++ main.go
@@ -7,19 +7,19 @@ var child2 r.Child2D
 
 func main() {
 	c := r.EngineConfig{
-		ScreenWidth:  1920,
-		ScreenHeight: 1080,
-		WindowTitle:  "game",
-		PolygonLines: false,
-		Dimensions:   2,
+		ScreenWidth:    1920,
+		ScreenHeight:   1080,
+		WindowTitle:    "game",
+		PolygonLines:   false,
+		CollisionLines: true,
+		Dimensions:     2,
 	}
 
-	engine := r.NewEngine(c)
+	engine := r.NewEngine(c, render)
 	err := engine.Initialize()
 	if err != nil {
 		panic(err)
 	}
-	engine.SetRenderFunc(render)
 
 	child1 = engine.NewChild2D()
 	child1.AttachPrimitive(r.NewRectangle(100, 100, &c))
@@ -36,14 +36,22 @@ func main() {
 	engine.Instance(&child1)
 	engine.Instance(&child2)
 
+	engine.CollisionControl.CreateGroup("children")
+	engine.CollisionControl.AddChildToGroup(&child1, "children")
+	engine.CollisionControl.AddChildToGroup(&child2, "children")
+	engine.CollisionControl.CreateCollision(&child1, "children", cbk)
+
+	child1.SetVelocity(1, 0)
+	child1.AttachGravity(0.2)
+
 	engine.StartRenderer()
 	<-engine.Done()
 }
 
 func render(renderer *r.Renderer) {
 	renderer.RenderChildren()
-	child1.SetPosition(child1.X+1, 500)
-	if child1.CheckCollision(&child2) {
-		println("yeet")
-	}
+}
+
+func cbk() {
+	println("YEETED")
 }
