b55b0f66f15d21c6b393b6e204cf681674bf439c
Rishabh Bector <bector2013@gmail.com>
2018-08-26 10:23:50 -0500

    Cleanup

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import r "RapidEngine"
 
+var child1 r.Child2D
+
 func main() {
 	c := r.EngineConfig{
 		ScreenWidth:  1920,
@@ -18,12 +20,18 @@ func main() {
 	}
 	engine.SetRenderFunc(render)
 
-	child1 := engine.NewChild2D()
+	child1 = engine.NewChild2D()
 	child1.AttachPrimitive(r.NewRectangle(100, 100, &c))
-	child1.AttachTexturePrimitive("./texture.png")
+	child1.AttachTexturePrimitive("./krita/blueSword.jpeg")
 	child1.SetPosition(500, 500)
 
+	child2 := engine.NewChild2D()
+	child2.AttachPrimitive(r.NewRectangle(100, 100, &c))
+	child2.AttachTexturePrimitive("./krita/goldSword.jpeg")
+	child2.SetPosition(600, 500)
+
 	engine.Instance(&child1)
+	engine.Instance(&child2)
 
 	engine.StartRenderer()
 	<-engine.Done()
@@ -31,4 +39,5 @@ func main() {
 
 func render(renderer *r.Renderer) {
 	renderer.RenderChildren()
+	child1.SetPosition(child1.X+1, 500)
 }
