02aa9ead9ae3088f776cc261f08d03cd83421969
Rishabh Bector <bector2013@gmail.com>
2018-09-08 15:27:54 -0500

    Stone generation

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	_ "net/http/pprof"
 	"rapidengine"
 	"rapidengine/configuration"
@@ -24,9 +22,6 @@ func init() {
 }
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:8080", nil))
-	}()
 	config = rapidengine.NewEngineConfig(1920, 1080, 2)
 	engine = rapidengine.NewEngine(config, render)
 
@@ -37,13 +32,16 @@ func main() {
 	engine.TextureControl.NewTexture("./assets/player/player.png", "player")
 	engine.TextureControl.NewTexture("./assets/blocks/dirt.png", "dirt")
 	engine.TextureControl.NewTexture("./assets/blocks/grass.png", "grass")
+	engine.TextureControl.NewTexture("./assets/blocks/stone.png", "stone")
 	engine.TextureControl.NewTexture("./assets/back.png", "back")
 
 	blocks = append(blocks, "player")
 	blocks = append(blocks, "dirt")
 	blocks = append(blocks, "grass")
+	blocks = append(blocks, "stone")
 
 	WorldChild = engine.NewChild2D()
+	WorldChild.AttachShader(engine.ShaderControl.GetShader("texture"))
 	WorldChild.AttachPrimitive(rapidengine.NewRectangle(BlockSize, BlockSize, &config))
 	WorldChild.AttachTexturePrimitive(engine.TextureControl.GetTexture("grass"))
 	WorldChild.EnableCopying()
@@ -57,9 +55,11 @@ func main() {
 	engine.CollisionControl.AddChildToGroup(&WorldChild, "ground")
 
 	Player = engine.NewChild2D()
+	Player.AttachShader(engine.ShaderControl.GetShader("texture"))
 	Player.AttachPrimitive(rapidengine.NewRectangle(30, 50, &config))
 	Player.AttachTexturePrimitive(engine.TextureControl.GetTexture("player"))
 	Player.SetPosition(3000, 20000)
+	//Player.SetPosition(-50, 0)
 	Player.AttachCollider(0, 0, 30, 50)
 	Player.SetGravity(1)
 
@@ -69,6 +69,7 @@ func main() {
 	Player.AddFrame(engine.TextureControl.GetTexture("player"))
 
 	SkyChild = engine.NewChild2D()
+	SkyChild.AttachShader(engine.ShaderControl.GetShader("texture"))
 	SkyChild.AttachPrimitive(rapidengine.NewRectangle(2000, 1150, &config))
 	SkyChild.AttachTexturePrimitive(engine.TextureControl.GetTexture("back"))
 
@@ -77,15 +78,15 @@ func main() {
 	engine.Instance(&Player)
 
 	engine.Initialize()
-
 	engine.StartRenderer()
 	<-engine.Done()
+	return
 }
 
 func render(renderer *rapidengine.Renderer, inputs *input.Input) {
+	movePlayer(inputs.Keys)
 	renderer.MainCamera.SetPosition(Player.X, Player.Y, 0)
 	SkyChild.SetPosition(Player.X-1950/2, Player.Y-1110/2)
-	movePlayer(inputs.Keys)
 }
 
 func movePlayer(keys map[string]bool) {
@@ -93,9 +94,9 @@ func movePlayer(keys map[string]bool) {
 		Player.SetVelocityY(10)
 	}
 	if keys["a"] {
-		Player.SetVelocityX(5)
+		Player.SetVelocityX(7)
 	} else if keys["d"] {
-		Player.SetVelocityX(-5)
+		Player.SetVelocityX(-7)
 	} else {
 		Player.SetVelocityX(0)
 	}
--- world.go
+++ world.go
@@ -7,30 +7,59 @@ import (
 	perlin "github.com/aquilax/go-perlin"
 )
 
+//  --------------------------------------------------
+//  World Generation Parameters
+//  --------------------------------------------------
+
 const WorldHeight = 1000
 const WorldWidth = 1000
 const BlockSize = 25
 const Flatness = 0.3
 
+const GrassMinimum = 500
+
 const CaveNoiseScalar = 30
 const CaveNoiseThreshold = 0.75
 
+const StoneNoiseScalar = 30.0
+const StoneTop = 500.0
+const StoneTopDeviation = 5
+const StoneStartingFrequency = -0.3
+
+//  --------------------------------------------------
+//  --------------------------------------------------
+//  --------------------------------------------------
+
 var p *perlin.Perlin
 var WorldMap [WorldWidth][WorldHeight]int
+var HeightMap [WorldWidth]int
 
 func generateWorld() {
 	p = perlin.NewPerlin(2, 2, 10, int64(rand.Int()))
 
+	// Fill world with 0s
 	for x := 0; x < WorldWidth; x++ {
 		for y := 0; y < WorldHeight; y++ {
 			WorldMap[x][y] = 0
 		}
 	}
-	heights := generateHeights()
+
+	// Generate heightmap and place grass
+	generateHeights()
 	for x := 0; x < WorldWidth; x++ {
-		WorldMap[x][heights[x]] = 2
+		WorldMap[x][HeightMap[x]] = 2
 	}
+
+	// Fill everything underneath grass with dirt
 	fillHeights()
+
+	// Generate stone based on height
+	fillStone()
+
+	// Clean up stone above ground
+	cleanStone()
+
+	// Generate caves
 	generateCaves()
 }
 
@@ -38,26 +67,14 @@ func createCopies() {
 	for x := 0; x < WorldWidth; x++ {
 		for y := 0; y < WorldHeight; y++ {
 			if WorldMap[x][y] != 0 {
-				WorldChild.AddCopy(rapidengine.ChildCopy{float32(x * BlockSize), float32(y * BlockSize), engine.TextureControl.GetTexture(blocks[WorldMap[x][y]])})
+				WorldChild.AddCopy(rapidengine.ChildCopy{
+					float32(x * BlockSize),
+					float32(y * BlockSize),
+					engine.TextureControl.GetTexture(blocks[WorldMap[x][y]]),
+				})
 			}
 		}
 	}
-}
-
-func noise2D(x, y float64) float64 {
-	return (p.Noise2D(x, y) + 0.4) / 0.8
-}
-
-func noise1D(x float64) float64 {
-	return (p.Noise1D(x) + 0.4) / 0.8
-}
-
-func generateHeights() [WorldWidth]int {
-	heights := [WorldWidth]int{}
-	for x := 0; x < WorldWidth; x++ {
-		heights[x] = 500 + int(Flatness*noise1D(float64(x)/WorldWidth)*WorldHeight)
-	}
-	return heights
 }
 
 func generateCaves() {
@@ -72,6 +89,12 @@ func generateCaves() {
 	}
 }
 
+func generateHeights() {
+	for x := 0; x < WorldWidth; x++ {
+		HeightMap[x] = GrassMinimum + int(Flatness*noise1D(float64(x)/WorldWidth)*WorldHeight)
+	}
+}
+
 func fillHeights() {
 	for x := 0; x < WorldWidth; x++ {
 		for y := 0; y < WorldHeight; y++ {
@@ -81,4 +104,43 @@ func fillHeights() {
 			}
 		}
 	}
+}
+
+func fillStone() {
+	p = perlin.NewPerlin(1.2, 2, 2, int64(rand.Int()))
+	stoneFrequency := StoneStartingFrequency
+	for y := 0; y < WorldHeight; y++ {
+		for x := 0; x < WorldWidth; x++ {
+			n := noise2D(StoneNoiseScalar*float64(x)/WorldWidth, StoneNoiseScalar*float64(y)/WorldHeight)
+			if n > stoneFrequency {
+				WorldMap[x][y] = 3
+			}
+		}
+		stoneFrequency += (1 / StoneTop)
+	}
+}
+
+func cleanStone() {
+	for x := 0; x < WorldWidth; x++ {
+		grassHeight := HeightMap[x]
+		if WorldMap[x][grassHeight] == 3 {
+			if WorldMap[x][grassHeight+StoneTopDeviation] == 0 {
+				for y := grassHeight + StoneTopDeviation; y < WorldHeight; y++ {
+					WorldMap[x][y] = 0
+				}
+			}
+		} else {
+			for y := grassHeight + 1; y < WorldHeight; y++ {
+				WorldMap[x][y] = 0
+			}
+		}
+	}
+}
+
+func noise2D(x, y float64) float64 {
+	return (p.Noise2D(x, y) + 0.4) / 0.8
+}
+
+func noise1D(x float64) float64 {
+	return (p.Noise1D(x) + 0.4) / 0.8
 }
